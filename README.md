c6a1c99e937fda31d2cd41ebd434f44a57071b3a
Roli <kuroridoplayer@gmail.com>
2024-10-27 21:59:11 +0800

    add example

--- README.md
+++ README.md
@@ -10,3 +10,35 @@ npm run dev
 # Publish
 
 npm publish
+
+# Usages
+
+in your global layout add the component `AppDrawer`
+
+```
+<script setup>
+import { AppDrawer } from 'digitspark-drawer'
+</script>
+```
+
+```
+<template>
+  <AppDrawer />
+</template>
+```
+
+in other component import the useAppDrawer composable
+
+```
+<script setup>
+import { useAppDrawer } from 'digitspark-drawer'
+
+const $drawer = useAppDrawer()
+
+function doPush() {
+  $drawer.push({
+    component: defineAsyncComponent(() => import('~/components/DrawerContent.vue'))
+  })
+}
+</script>
+```
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@
 import './style.css'
 import useAppDrawer from './composables/useAppDrawer';
 import AppDrawer from './components/AppDrawer.vue';
+import AppDrawerInner from './components/AppDrawerInner.vue';
 
 // Install function to allow the component to be registered globally
 const install = (app) => {
-  app.component('AppDrawer', AppDrawer);
   app.component('AppDrawerInner', AppDrawerInner);
+  app.component('AppDrawer', AppDrawer);
+  app.provide('useAppDrawer', useAppDrawer);
 };
 
 // Export the component and install function
 export default { install };
-export { AppDrawer, useAppDrawer };
+export { AppDrawer, AppDrawerInner, useAppDrawer };
